e9c8202dccce510d34df9ae6353d0f3dff8183bd
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-05-16 17:58:00 +0200

    first form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,9 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
-import { AppComponent } from './app.component';
-import {FormsModule} from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
+import {AppComponent} from './app.component';
 
 @NgModule({
   declarations: [
@@ -13,10 +11,11 @@ import {HttpClientModule} from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
