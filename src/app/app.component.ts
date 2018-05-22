6b45e66a6773ac1a399ec7b9df6eca5e7f810953
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-05-22 12:18:26 +0200

    set, path and reset values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,17 +21,33 @@ export class AppComponent implements OnInit {
       'gender': new FormControl('male'),
       'hobbies': new FormArray([])
     });
-   // status and value changes
+    // status and value changes
     //  this.signupForm.valueChanges.subscribe(
     //      (value) => console.log(value)
     //  );
     //  this.signupForm.statusChanges.subscribe(
     //    (value) => console.log(value)
     //  );
+    this.signupForm.setValue({
+        'userData': {
+          'username': 'Max',
+          'email': 'max@test.com'
+        },
+        'gender': 'male',
+        'hobbies': []
+      }
+    );
+    this.signupForm.patchValue({
+        'userData': {
+          'username': 'Anna',
+        }
+      }
+    );
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
